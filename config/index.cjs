dadc891d4e6719245f811a45c7c97621d89377e3
lJinhuan <112843320+lJinhuan@users.noreply.github.com>
2022-11-09 17:58:08 +0800

    upd_修改数据

--- config/index.cjs
+++ config/index.cjs
@@ -50,16 +50,7 @@ const USER_CONFIG = {
   ],
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0O1fXEct5W71b06bDo2eHHbh80nGi1gguo5piRn9ebA',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oJlid6W_9puzmBAL1Ky-pMShJAKk',
-    }
-  ],
 
 }
 
