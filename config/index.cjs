381eac1e138eecbcd50ea41aa4d6b15a3360f240
lJinhuan <112843320+lJinhuan@users.noreply.github.com>
2022-11-10 13:47:10 +0800

    UPD

--- config/index.cjs
+++ config/index.cjs
@@ -42,6 +42,31 @@ const USER_CONFIG = {
         { keyword: 'love_day', date: '2021-06-06' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '宝宝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oJlid6W_9puzmBAL1Ky-pMShJAKk',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0O1fXEct5W71b06bDo2eHHbh80nGi1gguo5piRn9ebA',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '12-27',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '2001', date: '01-2',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '节日', name: '相识纪念日', year: '2021', date: '06-06',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-06-06' },
+      ],
+    },
   ],
   CALLBACK_USERS: [
     {
